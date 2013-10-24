a79f792bad0a436321ae9fe91652e56a856b78a8
Patrick Carpenter <patrick@patrick.puppetlabs.vm>
2013-10-24 07:35:12 +0000

    Changed netinstall.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,5 @@
 # Install N-Able from its tarball, only works on RHEL5/6 or Centos 5/6
 class nable {
 
-  $nable_act_key,
-
-# Install tgz from source url
-nable::netinstall { nagent:
-  url                 => "https://uptime.xantrion.com/download/9.1.0.564/rhel5.1_64/N-central/nagent-rhel5.1_64.tar.gz",
-  extracted_dir       => "nagent-rhel5.1_64",
-  destination_dir     => "/var/tmp",
-  postextract_command => "$destination_dir/$extracted_dir/install.sh -k $nable_act_key"
-  creates             => "/etc/nagent.conf"
-}
-
-
-
 
 }
--- tests/init.pp
+++ tests/init.pp
@@ -1,2 +1,1 @@
 include nable
-  nable_act_key => 'aHR0cHM6Ly91cHRpbWUueGFudHJpb24uY29tOjQ0M3w2MTc3OTgwODd8MXww'
